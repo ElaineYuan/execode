87c7a28e6014d7546aa29f20a3bc659fa188f5b0
Pian <Pian@Pian-PC>
2013-09-18 22:11:19 +0800

    感谢大神

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,19 @@ public class Main {
 	{
 		Scanner input=new Scanner(System.in);
 		String str = input.next().toString(); 
-	//	char[]arr=str.toCharArray();
 		
 		String[]sarr=str.split("\\+|-|\\*|/");//numbersarr[0]
 		String[]sarr1=str.split("\\d+");//sarr1[0]里面为空的，从sarr[1]开始赋值
 		
 		int []num=new int[sarr.length];//把String数组转化成int型数组
-		//int []num1=num=new int[sarr.length];
-		//String[]ch=new String[sarr1.length];
 		int index=sarr1.length;
 		
 		for(int i=0;i<sarr.length;i++){
 			num[i]=Integer.parseInt(sarr[i]);		
 		}
 		
-		for(int i=1;i<index;i++){
-			if(sarr1[i].equals("*")||sarr1[i].equals("/")){
-				
+		for(int i=1;i<index;){			
+			if(sarr1[i].equals("*")||sarr1[i].equals("/")){				
 				if(sarr1[i].equals("*")){
 					num[i-1]=num[i-1]*num[i];
 					for(int j=i;j<index-1;j++){
@@ -42,9 +38,11 @@ public class Main {
 					
 				}
 				
-			}			
+			}else{
+				i++;
+				}
 		}
-		for(int i=1;i<index;i++){
+		for(int i=1;i<index;){
 			if(sarr1[i].equals("+")||sarr1[i].equals("-")){
 				
 				if(sarr1[i].equals("+")){
